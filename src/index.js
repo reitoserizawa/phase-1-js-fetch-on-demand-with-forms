cbd2bb29aa50ee485fd21e9b19a41293cca00353
ReitoSerizawa <s.reitiger@gmail.com>
2022-04-12 16:51:23 -0400

    commit tree e64e7073c9c7b9ac147a72050bb565c59f767744

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 const init = () => {
-  
+    const form = document.querySelector('form')
+    
+    form.addEventListener('submit', function (e) {
+    e.preventDefault()
+    const targetId = e.target.searchByID.value
+    
+    fetch(`http://localhost:3000/movies/${targetId}`)
+    .then(data => data.json())
+    .then(dataInside => {
+        
+    const h4 = document.querySelector('section#movieDetails h4')
+    const p = document.querySelector('section#movieDetails p')
+
+    h4.innerText = dataInside.title
+    p.innerText = dataInside.summary
+
+    form.reset()
+
+    })
+
+
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
